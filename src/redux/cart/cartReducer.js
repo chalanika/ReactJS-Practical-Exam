6aab69de8d12d9fb356bb4062178b1c0a54a12ea
chalanika sewwandi <43123589+chalanika@users.noreply.github.com>
2020-09-21 19:56:23 +0530

    Update cartReducer.js

--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -35,28 +35,7 @@ const cartReducer = (state = initialState, action) => {
       default: return state;
   }
 
-//   if (action.type === ADD_TO_CART) {
-//     let addedItem = state.items.find((item) => item.id === action.id);
-//     //check if the action id exists in the addedItems
-//     let existed_item = state.addedItems.find((item) => action.id === item.id);
-//     if (existed_item) {
-//       addedItem.quantity += 1;
-//       return {
-//         ...state,
-//         total: state.total + addedItem.price,
-//       };
-//     } else {
-//       addedItem.quantity = 1;
-//       //calculating the total
-//       let newTotal = state.total + addedItem.price;
 
-//       return {
-//         ...state,
-//         addedItems: [...state.addedItems, addedItem],
-//         total: newTotal,
-//       };
-//     }
-//   }
 };
 
 export default cartReducer;
